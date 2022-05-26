9ba6a7b1dc925f6641bdce9dc31e0761f675ad3f
Magnus0214 <106291066+Magnus0214@users.noreply.github.com>
2022-05-26 20:26:19 +0800

    VR-Tour_Utown

---
+++ app-files/data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.857927366582061,
+        "pitch": -0.09246021923400782,
+        "fov": 1.3274289077235746
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.736298076313904,
+          "pitch": 0.11786311397442972,
+          "rotation": 0,
+          "target": "2-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.007325125362527274,
+          "pitch": 0.1383561309665371,
+          "title": "Greenery",
+          "text": "A touch of green in the walls of steel and concrete"
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5015980100058322,
+          "pitch": 0.024993304815179584,
+          "rotation": 0,
+          "target": "2-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6506504075969488,
+          "pitch": 0.05771159728409003,
+          "title": "Grab&amp;go",
+          "text": "A collection of vending machines"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.289792004916304,
+          "pitch": 0.035173772701869055,
+          "rotation": 0,
+          "target": "0-erc_level-1_tembusu-tree"
+        },
+        {
+          "yaw": -1.0221725178440515,
+          "pitch": 0.09108237086248216,
+          "rotation": 0,
+          "target": "1-erc_level-1_starbucks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.84998752486017,
+          "pitch": 0.027877029847655166,
+          "title": "U-Town",
+          "text": "Home to many NUS"
+        }
+      ]
+    }
+  ],
+  "name": "Assignments",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
